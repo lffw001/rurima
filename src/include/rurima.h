67d96b43847d2dd0a8d871f11ea7a84dd93669eb
Moe-hacker <moe-hacker@outlook.com>
2025-04-16 09:45:25 +0800

    Sync ruri update

--- src/include/rurima.h
+++ src/include/rurima.h
@@ -199,8 +199,6 @@ void check_dir_deny_list(const char *_Nonnull dir);
 char *strstr_ignore_case(const char *_Nonnull haystack, const char *_Nonnull needle);
 int fork_rexec(int argc, char **_Nonnull argv);
 void *default_hook(const char *_Nonnull container_dir);
-void list_template(void);
-void template(const char *_Nonnull name, const char *_Nonnull dir, int argc, char **argv);
 void read_global_config(void);
 bool rootless_supported(void);
 off_t get_dir_file_size(const char *_Nonnull target);
